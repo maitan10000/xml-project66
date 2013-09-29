395a138ae130ade67518c59737304fd621948ea7
tqbao60575 <tqbao60575@gmail.com>
2013-09-29 01:37:43 +0000

    test commit 6

---
+++ FastFoodWeb/src/java/FastFood/Common/Dao/CategoryDaoInterface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package FastFood.Common.Dao;
+import FastFood.Common.Bean.CategoryBean;
+import java.util.List;
+
+/**
+ *
+ * @author bao
+ */
+public interface CategoryDaoInterface {
+     public boolean Add(String Name);
+    public boolean Update(String Name);
+    public boolean Delete(String ID);
+    public CategoryBean ListByCatID(String ID);
+    public List<String> ListAllCatID();
+}
